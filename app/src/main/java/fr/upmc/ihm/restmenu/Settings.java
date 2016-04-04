e991951b55c9979d93fcf5400bd04e50e3d7a17b
AdelLarbi <larbi.adel@yahoo.fr>
2016-04-04 17:43:04 +0200

    Settings layout added

---
+++ app/src/main/java/fr/upmc/ihm/restmenu/Settings.java
@@ -0,0 +1,16 @@
+package fr.upmc.ihm.restmenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adel on 04/04/16.
+ */
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
