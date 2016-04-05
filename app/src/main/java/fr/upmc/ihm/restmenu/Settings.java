11b4d105fc350b609ef3c853909a139de56cb5a1
AdelLarbi <larbi.adel@yahoo.fr>
2016-04-05 02:20:19 +0200

    SecretCode added

--- app/src/main/java/fr/upmc/ihm/restmenu/Settings.java
+++ app/src/main/java/fr/upmc/ihm/restmenu/Settings.java
@@ -1,7 +1,9 @@
 package fr.upmc.ihm.restmenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by adel on 04/04/16.
@@ -12,5 +14,15 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+    }
+
+    public void selectTable(View view) {
+        Intent intent = new Intent(this, TableSelector.class);
+        startActivity(intent);
+    }
+
+    public void setCode(View view) {
+        Intent intent = new Intent(this, SecretCode.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/fr/upmc/ihm/restmenu/TableSelector.java
@@ -0,0 +1,47 @@
+package fr.upmc.ihm.restmenu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class TableSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.table_selector);
+    }
+
+    public void scan(View view) {
+        try {
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+
+                // Handle successful scan
+                Log.i("TableSelector","Scan result: " + contents);
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+                Log.i("TableSelector","Scan unsuccessful");
+            }
+        }
+    }
+}
